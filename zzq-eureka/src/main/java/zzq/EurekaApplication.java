92253128a94071044826283d769bf6cbc9dcc4dc
zzq <2044579211@qq.com>
2018-11-17 16:01:04 +0800

    项目初始化

---
+++ zzq-eureka/src/main/java/zzq/EurekaApplication.java
@@ -0,0 +1,11 @@
+package zzq;
+
+/**
+ * 〈功能简述〉<br>
+ * 〈服务注册中心启动类〉
+ *
+ * @author zhouzhiqiang
+ * @create 2018/11/17 0017
+ */
+public class EurekaApplication {
+}
---
+++ zzq-zuul/src/main/java/zzq/ZuulApplication.java
@@ -0,0 +1,11 @@
+package zzq;
+
+/**
+ * 〈功能简述〉<br>
+ * 〈网关启动类〉
+ *
+ * @author zhouzhiqiang
+ * @create 2018/11/17 0017
+ */
+public class ZuulApplication {
+}
