c6a2cb48b90fdadce29c13c4de1e2cf0e032960a
zzq <2044579211@qq.com>
2018-11-17 16:34:27 +0800

    zuul网关搭建

--- zzq-zuul/src/main/java/zzq/ZuulApplication.java
+++ zzq-zuul/src/main/java/zzq/ZuulApplication.java
@@ -1,5 +1,9 @@
 package zzq;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
 /**
  * 〈功能简述〉<br>
  * 〈网关启动类〉
@@ -7,5 +11,11 @@ package zzq;
  * @author zhouzhiqiang
  * @create 2018/11/17 0017
  */
+@SpringBootApplication
+@EnableZuulProxy
 public class ZuulApplication {
+    
+    public static void main(String[] args){
+        SpringApplication.run(ZuulApplication.class,args);
+    }
 }
