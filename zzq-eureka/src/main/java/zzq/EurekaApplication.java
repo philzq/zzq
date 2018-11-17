ec14c9cc9701744f93dc104be4e4d2ce9f9e4cc5
zzq <2044579211@qq.com>
2018-11-17 16:26:48 +0800

    服务注册中心搭建

--- zzq-eureka/src/main/java/zzq/EurekaApplication.java
+++ zzq-eureka/src/main/java/zzq/EurekaApplication.java
@@ -1,5 +1,9 @@
 package zzq;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
 /**
  * 〈功能简述〉<br>
  * 〈服务注册中心启动类〉
@@ -7,5 +11,11 @@ package zzq;
  * @author zhouzhiqiang
  * @create 2018/11/17 0017
  */
+@EnableEurekaServer  //启动一个服务注册中心提供给其他应用进行对话
+@SpringBootApplication
 public class EurekaApplication {
+
+    public static void main(String[] args){
+        SpringApplication.run(EurekaApplication.class,args);
+    }
 }
