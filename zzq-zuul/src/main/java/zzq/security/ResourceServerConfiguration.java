fda22b47da281746edb7bf9823831db262b1d24d
18770911080 <2044579211@qq.com>
2018-12-04 15:36:57 +0800

    Update ResourceServerConfiguration.java

--- zzq-zuul/src/main/java/zzq/security/ResourceServerConfiguration.java
+++ zzq-zuul/src/main/java/zzq/security/ResourceServerConfiguration.java
@@ -1,10 +1,13 @@
 package zzq.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.TokenStore;
 
 /**
  * 〈功能简述〉<br>
@@ -13,19 +16,21 @@ import org.springframework.security.oauth2.config.annotation.web.configurers.Res
  * @author zhouzhiqiang
  * @create 2018/11/18 0018
  */
-//@Configuration
-//@EnableResourceServer
+@Configuration
+@EnableResourceServer
 public class ResourceServerConfiguration extends ResourceServerConfigurerAdapter {
 
     @Override
     public void configure(ResourceServerSecurityConfigurer resources) {
+        // 如果关闭 stateless，则 accessToken 使用时的 session id 会被记录，后续请求不携带 accessToken 也可以正常响应
         resources.resourceId("admin").stateless(true);
     }
 
     @Override
     public void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
-            .antMatchers("/admin/**").authenticated();//配置order访问控制，必须认证过后才可以访问
-
+        http.antMatcher("/admin/**")
+                .authorizeRequests()
+                .antMatchers(HttpMethod.OPTIONS).permitAll()
+                .anyRequest().authenticated();
     }
 }
