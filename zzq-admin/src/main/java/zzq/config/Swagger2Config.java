6a5fa5c06a19c17702694c9ec03d9b04b71ef91c
zzq <2044579211@qq.com>
2018-12-04 20:20:32 +0800

    整合swagger2

---
+++ zzq-admin/src/main/java/zzq/config/Swagger2Config.java
@@ -0,0 +1,55 @@
+package zzq.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/**
+ * 〈功能简述〉<br>
+ * 〈swaggerConfig〉
+ *
+ * @author zhouzhiqiang
+ * @create 2018-12-04
+ */
+@Configuration
+@EnableSwagger2
+public class Swagger2Config {
+    /**
+     * swagger2的配置文件，这里可以配置swagger2的一些基本的内容，比如扫描的包等等
+     *
+     * @return Docket
+     */
+    @Bean
+    public Docket createRestApi() {
+        return new Docket(DocumentationType.SWAGGER_2).apiInfo(apiInfo()).select()
+                // 为当前包路径
+                .apis(RequestHandlerSelectors.basePackage("zzq.controller")).paths(PathSelectors.any())
+                .build();
+    }
+
+    /**
+     * api文档的详细信息函数,注意这里的注解引用的是哪个
+     *
+     * @return
+     */
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                // //大标题
+                .title("Admin RESTful API")
+                // 版本号
+                .version("0.0.1-SNAPSHOT")
+                // 描述
+                .description("API 描述")
+                //作者
+                .contact(new Contact("zhouzhiqiang", "https://blog.csdn.net/qq_33594101", "2044579211@qq.com"))
+                .build();
+    }
+}
+
