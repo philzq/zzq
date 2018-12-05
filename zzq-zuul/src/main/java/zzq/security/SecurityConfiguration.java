c11eec323b13adb8e33e46f4a36104d44c659d22
zzq <2044579211@qq.com>
2018-12-05 21:44:59 +0800

    优化

--- zzq-zuul/src/main/java/zzq/security/SecurityConfiguration.java
+++ zzq-zuul/src/main/java/zzq/security/SecurityConfiguration.java
@@ -1,15 +1,9 @@
 package zzq.security;
 
 import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
-import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.factory.PasswordEncoderFactories;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 /**
  * 〈功能简述〉<br>
