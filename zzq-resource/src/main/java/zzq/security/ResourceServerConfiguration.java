2e2a4cbce3076c2b447bb8fe7f1f71a9c6e7800b
zzq <2044579211@qq.com>
2018-12-05 20:24:59 +0800

    资源中心优化

--- zzq-resource/src/main/java/zzq/security/ResourceServerConfiguration.java
+++ zzq-resource/src/main/java/zzq/security/ResourceServerConfiguration.java
@@ -1,11 +1,20 @@
 package zzq.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.oauth2.common.DefaultOAuth2AccessToken;
+import org.springframework.security.oauth2.common.OAuth2AccessToken;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.OAuth2Authentication;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * 〈功能简述〉<br>
@@ -17,6 +26,39 @@ import org.springframework.security.oauth2.config.annotation.web.configurers.Res
 @Configuration
 @EnableResourceServer
 public class ResourceServerConfiguration extends ResourceServerConfigurerAdapter {
+
+    /**
+     * JWT token Converter
+     * @return
+     */
+    @Bean
+    public JwtAccessTokenConverter accessTokenConverter() {
+        JwtAccessTokenConverter accessTokenConverter = new JwtAccessTokenConverter() {
+            /***
+             *  增強token的方法,自訂義一些token返回的信息
+             */
+            @Override
+            public OAuth2AccessToken enhance(OAuth2AccessToken accessToken, OAuth2Authentication authentication) {
+                String userName = authentication.getUserAuthentication().getName();
+
+                System.out.println(">>>"+userName);
+
+                //登录时候放进去的一些用户信息
+                User user = (User) authentication.getUserAuthentication().getPrincipal();
+                /** 自定义一些token屬性 ***/
+                final Map<String, Object> additionalInformation = new HashMap<>();
+                additionalInformation.put("userName", userName);
+                additionalInformation.put("roles", user.getAuthorities());
+                ((DefaultOAuth2AccessToken) accessToken).setAdditionalInformation(additionalInformation);
+                OAuth2AccessToken enhancedToken = super.enhance(accessToken, authentication);
+                return enhancedToken;
+            }
+
+        };
+        //测试时，资源服务器使用相同的字符串得到一个对称加密的效果，生产时候使用RSA非对称加密方式
+        accessTokenConverter.setSigningKey("123");
+        return accessTokenConverter;
+    }
 
     @Override
     public void configure(ResourceServerSecurityConfigurer resources) {
