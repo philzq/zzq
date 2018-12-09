133f0995384c8b905c94c333cbea924f290c7d8f
zzq <2044579211@qq.com>
2018-12-09 11:02:55 +0800

    去掉多余的包

--- zzq-admin/src/main/java/zzq/service/UserService.java
+++ zzq-admin/src/main/java/zzq/service/UserService.java
@@ -3,7 +3,6 @@ package zzq.service;
 import com.baomidou.mybatisplus.extension.service.IService;
 import org.springframework.transaction.annotation.Transactional;
 import zzq.entity.User;
-import zzq.utils.R;
 
 /**
  * 〈功能简述〉<br>
--- zzq-admin/src/main/java/zzq/service/impl/UserServiceImpl.java
+++ zzq-admin/src/main/java/zzq/service/impl/UserServiceImpl.java
@@ -1,13 +1,10 @@
 package zzq.service.impl;
 
-import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
-import com.sun.org.apache.xerces.internal.util.EntityResolver2Wrapper;
 import org.springframework.stereotype.Service;
 import zzq.entity.User;
 import zzq.mapper.UserMapper;
 import zzq.service.UserService;
-import zzq.utils.R;
 
 /**
  * 〈功能简述〉<br>
