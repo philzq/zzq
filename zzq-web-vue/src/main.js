c2fafb29055646fd27998727b9fff39cacb59109
zhiqiangzhou <Zzq144091?>
2019-05-17 15:37:01 +0800

    修改项目访问路径

--- zzq-web-vue/src/main.js
+++ zzq-web-vue/src/main.js
@@ -7,7 +7,7 @@ import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
 import locale from 'element-ui/lib/locale/lang/zh-TW'
 
-Vue.use(Element, { locale ,size: 'small', zIndex: 3000 });
+Vue.use(ElementUI, { locale ,size: 'small', zIndex: 3000 });
 
 Vue.config.productionTip = false
 
