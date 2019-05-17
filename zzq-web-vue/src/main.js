e26754f2b3d55bfbed84c4600433da77af397c31
zhiqiangzhou <Zzq144091?>
2019-05-17 14:09:06 +0800

    引入element-ui框架

--- zzq-web-vue/src/main.js
+++ zzq-web-vue/src/main.js
@@ -3,6 +3,11 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+import locale from 'element-ui/lib/locale/lang/zh-TW'
+
+Vue.use(Element, { locale ,size: 'small', zIndex: 3000 });
 
 Vue.config.productionTip = false
 
