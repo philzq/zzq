c41899eeffb7fb832e0eff65e2b31f31e9d0012e
zhiqiangzhou <Zzq144091?>
2019-06-12 16:10:08 +0800

    slot插槽

--- zzq-web-vue/src/components/componentsSlot/childSlot.vue
+++ zzq-web-vue/src/components/componentsSlot/childSlot.vue
@@ -3,13 +3,11 @@
     <!--插槽的应用场景:主要用于多个父组件对于一个字组件的内容有不同的实现方式-->
     <!--匿名插槽-->
     <slot></slot>
-    <slot></slot>
     <!--具名插槽-->
     <slot name="test">
       child hello world
     </slot>
     <!--作用域插槽——带数据的插槽-->
-    <slot :data="data"></slot>
     <slot :data="data"></slot>
   </div>
 </template>
