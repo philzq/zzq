a3fe81fa6cac61050bb3787047ca59df265f1731
zhiqiangzhou <Zzq144091?>
2019-08-14 17:12:16 +0800

    設計模式實現的初步規劃

---
+++ zzq-java-demo/src/main/java/zzq/designpatterns/creativepattern/Prototype.java
@@ -0,0 +1,7 @@
+package zzq.designpatterns.creativepattern;
+
+/**
+ * 原型（Prototype）模式的定义如下：用一个已经创建的实例作为原型，通过复制该原型对象来创建一个和原型相同或相似的新对象。在这里，原型实例指定了要创建的对象的种类。用这种方式创建对象非常高效，根本无须知道对象创建的细节。例如，Windows 操作系统的安装通常较耗时，如果复制就快了很多。
+ */
+public class Prototype {
+}
