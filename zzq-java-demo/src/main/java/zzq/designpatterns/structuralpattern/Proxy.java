4bf4673109feb7959042101fd354da2383a19cf4
zhiqiangzhou <Zzq144091?>
2019-08-27 16:56:00 +0800

    代理模式

--- zzq-java-demo/src/main/java/zzq/designpatterns/structuralpattern/Proxy.java
+++ zzq-java-demo/src/main/java/zzq/designpatterns/structuralpattern/Proxy.java
@@ -1,16 +1,68 @@
 package zzq.designpatterns.structuralpattern;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
 /**
- * 代理模式的定义：由于某些原因需要给某对象提供一个代理以控制对该对象的访问。这时，访问对象不适合或者不能直接引用目标对象，代理对象作为访问对象和目标对象之间的中介。
- *
+ * 代理模式的定义：由于某些原因需要给某对象提供一个代理以控制对该对象的访问。
+ * 这时，访问对象不适合或者不能直接引用目标对象，代理对象作为访问对象和目标对象之间的中介。
+ * <p>
  * 代理模式的主要优点有：
  * 代理模式在客户端与目标对象之间起到一个中介作用和保护目标对象的作用；
  * 代理对象可以扩展目标对象的功能；
  * 代理模式能将客户端与目标对象分离，在一定程度上降低了系统的耦合度；
- *
+ * <p>
  * 其主要缺点是：
  * 在客户端和目标对象之间增加一个代理对象，会造成请求处理速度变慢；
  * 增加了系统的复杂度；
+ * <p>
+ * 代理又分静态代理与动态代理，（此处演示动态代理）
+ * 静态代理:由程序员创建或工具生成代理类的源码，再编译代理类。
+ * 所谓静态也就是在程序运行前就已经存在代理类的字节码文件，代理类和委托类的关系在运行前就确定了。
+ * <p>
+ * 动态代理:动态代理类的源码是在程序运行期间由JVM根据反射等机制动态的生成，
+ * 所以不存在代理类的字节码文件。代理类和委托类的关系是在程序运行时确定。
  */
 public class Proxy {
+
+    public interface IHello {
+        void sayHello();
+    }
+
+    static class Hello implements IHello {
+        public void sayHello() {
+            System.out.println("Hello world!!");
+        }
+    }
+
+    //自定义InvocationHandler
+    static class HWInvocationHandler implements InvocationHandler {
+        //目标对象
+        private Object target;
+
+        public HWInvocationHandler(Object target) {
+            this.target = target;
+        }
+
+        public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+            System.out.println("------插入前置通知代码-------------");
+            //执行相应的目标方法
+            Object rs = method.invoke(target, args);
+            System.out.println("------插入后置处理代码-------------");
+            return rs;
+        }
+    }
+
+    public static void main(String[] args) throws NoSuchMethodException, IllegalAccessException, InvocationTargetException, InstantiationException {
+        //获取动态代理类
+        Class proxyClazz = java.lang.reflect.Proxy.getProxyClass(IHello.class.getClassLoader(), IHello.class);
+        //获得代理类的构造函数，并传入参数类型InvocationHandler.class
+        Constructor constructor = proxyClazz.getConstructor(InvocationHandler.class);
+        //通过构造函数来创建动态代理对象，将自定义的InvocationHandler实例传入
+        IHello iHello = (IHello) constructor.newInstance(new HWInvocationHandler(new Hello()));
+        //通过代理对象调用目标方法
+        iHello.sayHello();
+    }
 }
