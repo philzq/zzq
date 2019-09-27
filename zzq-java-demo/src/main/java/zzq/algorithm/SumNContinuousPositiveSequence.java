a90fce9758fed8383df253a3ee3e54355b7afa2f
zhiqiangzhou <Zzq144091?>
2019-09-27 14:10:50 +0800

    算法训练-和为n 连续正数序列

--- zzq-java-demo/src/main/java/zzq/algorithm/SumNContinuousPositiveSequence.java
+++ zzq-java-demo/src/main/java/zzq/algorithm/SumNContinuousPositiveSequence.java
@@ -5,7 +5,9 @@ import java.util.List;
 
 /**
  * 和为n 连续正数序列
- * 输入一个正数n，输出所有和为n 连续正数序列
+ *
+ * 题目：输入一个正数n，输出所有和为n 连续正数序列。
+ * 例如输入15，由于1+2+3+4+5=4+5+6=7+8=15，所以输出3 个连续序列1-5、4-6 和7-8。
  */
 public class SumNContinuousPositiveSequence {
 
