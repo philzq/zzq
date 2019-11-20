8ace8b385b313e7285c024341e3471e5c2e9f2be
zhiqiangzhou <Zzq144091?>
2019-11-20 17:16:32 +0800

    优化

--- simple-mybatis/src/main/java/zzq/simple/mybatis/TestMybatis.java
+++ simple-mybatis/src/main/java/zzq/simple/mybatis/TestMybatis.java
@@ -19,33 +19,32 @@ import java.util.ResourceBundle;
 public class TestMybatis {
 
     public static void main(String[] args) throws Exception {
+        /****************************初始化过程**********************************/
         Configuration configuration = new Configuration();
-
         ResourceBundle resourceBundle = ResourceBundle.getBundle("mybatis-config");
-
         //加载数据源
         MariaDbDataSource mariaDbDataSource = new MariaDbDataSource();
         mariaDbDataSource.setUrl(resourceBundle.getString("jdbc.url"));
         mariaDbDataSource.setUserName(resourceBundle.getString("jdbc.userName"));
         mariaDbDataSource.setPassword(resourceBundle.getString("jdbc.passWord"));
-        JdbcTransaction jdbcTransaction = new JdbcTransaction(mariaDbDataSource);
-
         //加载接口
         ClassPathMapperScanner classPathMapperScanner = new ClassPathMapperScanner(configuration);
         classPathMapperScanner.doScanMapper();
-
         //加载mapper.xml
         XMLMapperBuilder xmlMapperBuilder = new XMLMapperBuilder(configuration);
         Resource[] mapperLocations = new PathMatchingResourcePatternResolver()
                 .getResources(resourceBundle.getString("mapperLocation"));
         xmlMapperBuilder.readMapper(mapperLocations);
 
+        /*****************************执行过程**********************************/
+        //创建JdbcTransaction事物实例
+        JdbcTransaction jdbcTransaction = new JdbcTransaction(mariaDbDataSource);
+        //创建执行器实例
+        Executor executor = new Executor(jdbcTransaction);
         //创建SqlSession实例
-        SqlSession sqlSession = new SqlSession(configuration,new Executor(jdbcTransaction));
-
+        SqlSession sqlSession = new SqlSession(configuration, executor);
         //获取代理Mapper对象
         OrderLogMapper orderLogMapper = sqlSession.getMapper(OrderLogMapper.class);
-
         //根据日期id获取指定的日志
         OrderLog orderLog = orderLogMapper.getOrderLogByLogID(9000L);
         System.out.println(orderLog);
