e485fbb3d1de5cc14ac6819883b7938af0ef7ea0
zhiqiangzhou <Zzq144091?>
2019-11-20 13:20:44 +0800

    mybatis-test表

---
+++ simple-mybatis/src/main/resources/db/db.sql
@@ -0,0 +1,23 @@
+create table `OrderLog` (
+	`LogID` bigint (20)
+); 
+insert into `OrderLog` (`LogID`) values('1');
+insert into `OrderLog` (`LogID`) values('2');
+insert into `OrderLog` (`LogID`) values('3');
+insert into `OrderLog` (`LogID`) values('4');
+insert into `OrderLog` (`LogID`) values('5');
+insert into `OrderLog` (`LogID`) values('6');
+insert into `OrderLog` (`LogID`) values('7');
+insert into `OrderLog` (`LogID`) values('8');
+insert into `OrderLog` (`LogID`) values('10');
+insert into `OrderLog` (`LogID`) values('20');
+insert into `OrderLog` (`LogID`) values('21');
+insert into `OrderLog` (`LogID`) values('22');
+insert into `OrderLog` (`LogID`) values('23');
+insert into `OrderLog` (`LogID`) values('24');
+insert into `OrderLog` (`LogID`) values('25');
+insert into `OrderLog` (`LogID`) values('26');
+insert into `OrderLog` (`LogID`) values('27');
+insert into `OrderLog` (`LogID`) values('28');
+insert into `OrderLog` (`LogID`) values('29');
+insert into `OrderLog` (`LogID`) values('30');
