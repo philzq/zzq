9e8370e9784480edd5db3eee7cf500e709ea2a65
zhiqiangzhou <Zzq144091?>
2019-11-20 13:18:41 +0800

    mybatis框架优化

--- simple-mybatis/src/main/java/zzq/simple/main/entity/OrderLog.java
+++ simple-mybatis/src/main/java/zzq/simple/main/entity/OrderLog.java
@@ -17,42 +17,4 @@ public class OrderLog {
      */
     @ApiModelProperty(value = "日志ID:")
     private Long logID;
-
-    /**
-     * 订单号 : 
-     */
-    @ApiModelProperty(value = "订单号:")
-    private String orderNo;
-
-    /**
-     * 操作员 : 
-     */
-    @ApiModelProperty(value = "操作员:")
-    private String operator;
-
-    /**
-     * 操作时间 : 
-     */
-    @ApiModelProperty(value = "操作时间:")
-    private LocalDateTime operateTime;
-
-    /**
-     * GUID : 
-     */
-    @ApiModelProperty(value = "GUID:")
-    private String sendGUID;
-
-    private Integer sendType;
-
-    /**
-     * 操作内容 : 
-     */
-    @ApiModelProperty(value = "操作内容:")
-    private String operateContent;
-
-    /**
-     * 系统备注 : 
-     */
-    @ApiModelProperty(value = "系统备注:")
-    private String systemRemark;
 }
