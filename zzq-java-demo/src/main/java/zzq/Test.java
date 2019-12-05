37aa076a876ba0bd711dba27b34301a62da47f5d
zhiqiangzhou <Zzq144091?>
2019-12-05 14:44:38 +0800

    test

---
+++ zzq-java-demo/src/main/java/zzq/Test.java
@@ -0,0 +1,9 @@
+package zzq;
+
+public class Test {
+
+    public static void main(String[] args){
+        System.out.println(Integer.MAX_VALUE);
+        System.out.println(Integer.MAX_VALUE+1);
+    }
+}
