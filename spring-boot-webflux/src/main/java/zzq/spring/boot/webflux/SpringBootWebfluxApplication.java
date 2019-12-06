db1fe61e7966f432378973f9aa604b61373a2874
zhiqiangzhou <Zzq144091?>
2019-12-06 18:04:49 +0800

    spring-boot-webflux

--- spring-boot-webflux/src/main/java/zzq/spring/boot/webflux/SpringBootWebfluxApplication.java
+++ spring-boot-webflux/src/main/java/zzq/spring/boot/webflux/SpringBootWebfluxApplication.java
@@ -1,10 +1,15 @@
 package zzq.spring.boot.webflux;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.ServerProperties;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.WebFilter;
 import reactor.core.publisher.Mono;
 
 @SpringBootApplication
@@ -12,8 +17,26 @@ import reactor.core.publisher.Mono;
 @EnableEurekaClient
 public class SpringBootWebfluxApplication {
 
+    @Autowired
+    private ServerProperties serverProperties;
+
     public static void main(String[] args){
         SpringApplication.run(SpringBootWebfluxApplication.class,args);
+    }
+
+    @Bean
+    public WebFilter contextPathWebFilter() {
+        String contextPath = serverProperties.getServlet().getContextPath();
+        return (exchange, chain) -> {
+            ServerHttpRequest request = exchange.getRequest();
+            if (request.getURI().getPath().startsWith(contextPath)) {
+                return chain.filter(
+                        exchange.mutate()
+                                .request(request.mutate().contextPath(contextPath).build())
+                                .build());
+            }
+            return chain.filter(exchange);
+        };
     }
 
     @GetMapping("/getTestStr")
