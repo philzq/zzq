e90d79ad48cc1fca31cf28dec010f4f3b826bf0c
zhiqiangzhou <Zzq144091?>
2019-12-06 11:27:55 +0800

    优化

--- spring-boot-web-jetty-container/src/main/java/zzq/spring/boot/web/jetty/container/SpringBootWebJettyContainer.java
+++ spring-boot-web-jetty-container/src/main/java/zzq/spring/boot/web/jetty/container/SpringBootWebJettyContainer.java
@@ -2,11 +2,13 @@ package zzq.spring.boot.web.jetty.container;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
+@EnableEurekaClient
 public class SpringBootWebJettyContainer {
 
     public static void main(String[] args){
--- spring-boot-web-tomcat-container/src/main/java/zzq/spring/boot/web/tomcat/container/SpringBootWebTomcatContainer.java
+++ spring-boot-web-tomcat-container/src/main/java/zzq/spring/boot/web/tomcat/container/SpringBootWebTomcatContainer.java
@@ -2,11 +2,13 @@ package zzq.spring.boot.web.tomcat.container;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
+@EnableEurekaClient
 public class SpringBootWebTomcatContainer {
 
     public static void main(String[] args){
