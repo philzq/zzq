2c7d1cad2a95f4ecd54be89f4643cf2fc805c0eb
zhiqiangzhou <Zzq144091?>
2019-12-20 09:36:13 +0800

    thread

--- zzq-micro-service/zzq-java-demo/src/main/java/zzq/thread/ArrayBlockingQueueTest.java
+++ zzq-micro-service/zzq-java-demo/src/main/java/zzq/thread/ArrayBlockingQueueTest.java
@@ -6,6 +6,13 @@ import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
 
+/**
+ * 线程池执行任务流程
+ * 1.当线程数小于线程池的最小线程数时，会创建线程去执行任务
+ * 2.当线程池的线程数大于等于线程池的最小线程数时，如果线程池处于运行状态，且阻塞队列没满时，则将任务塞进队列
+ * 3.如果队列满时，但线程池的线程数小于最大线程数时，直接创建线程去处理任务
+ * 4.否则拒绝任务
+ */
 public class ArrayBlockingQueueTest {
 
     public static void main(String[] args) throws Exception{
