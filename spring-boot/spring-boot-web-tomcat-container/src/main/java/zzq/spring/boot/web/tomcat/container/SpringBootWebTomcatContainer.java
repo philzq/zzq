bb2366d52bb5a43db9b358ba9a3d0f69bb77acec
zhiqiangzhou <Zzq144091?>
2019-12-23 11:21:21 +0800

    Tomcat配置详解

--- spring-boot/spring-boot-web-tomcat-container/src/main/java/zzq/spring/boot/web/tomcat/container/SpringBootWebTomcatContainer.java
+++ spring-boot/spring-boot-web-tomcat-container/src/main/java/zzq/spring/boot/web/tomcat/container/SpringBootWebTomcatContainer.java
@@ -2,13 +2,11 @@ package zzq.spring.boot.web.tomcat.container;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
-@EnableEurekaClient
 public class SpringBootWebTomcatContainer {
 
     public static void main(String[] args){
@@ -17,7 +15,7 @@ public class SpringBootWebTomcatContainer {
 
     @GetMapping("getTestStr")
     public String getTestStr() throws Exception{
-        Thread.sleep(3000);
-        return "Container";
+        Thread.sleep(5000);
+        return "Container啊啊";
     }
 }
