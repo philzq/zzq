7d4790475d8efd85f967b24a73fb1c9f5e35dfc3
zhiqiangzhou <zhiqiangzhou@ctrip.com>
2020-04-16 14:06:55 +0800

    jacoco--初步实现

---
+++ spring-boot/spring-boot-sonarqube-jacoco/src/main/java/zzq/springboot/sonarqubejacoco/SonarQubeJacocoApplication.java
@@ -0,0 +1,12 @@
+package zzq.springboot.sonarqubejacoco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SonarQubeJacocoApplication {
+    
+    public static void main(String[] args){
+        SpringApplication.run(SonarQubeJacocoApplication.class, args);
+    }
+}
---
+++ spring-boot/spring-boot-sonarqube-jacoco/src/test/java/zzq/springboot/sonarqubejacoco/SonarQubeJacocoApplicationTest.java
@@ -0,0 +1,19 @@
+package zzq.springboot.sonarqubejacoco;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.assertTrue;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SonarQubeJacocoApplicationTest {
+
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
