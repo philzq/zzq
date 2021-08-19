bbbf1770b79a95f7c9bd5541d0d02175ee34df7e
zhiqiangzhou <zhiqiangzhou@ctrip.com>
2021-08-19 17:38:09 +0800

    mq

--- spring-boot/spring-boot-ribbitmq/src/main/java/zzq/consumption/RabbitMqMessage.java
+++ spring-boot/spring-boot-ribbitmq/src/main/java/zzq/consumption/RabbitMqMessage.java
@@ -1,30 +1,37 @@
 package zzq.consumption;
 
+import com.rabbitmq.client.Channel;
+import org.springframework.amqp.core.Message;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
 
+import java.io.IOException;
 import java.time.LocalDateTime;
 
 @Component
 public class RabbitMqMessage {
 
-    @RabbitListener(queues = "testFirst", containerFactory="firstFactory")
-    public void testFirst(Object data) {
+    @RabbitListener(queues = "testFirst", containerFactory = "firstFactory")
+    public void testFirst(Channel channel, Message message) throws IOException {
         try {
-            System.out.println(LocalDateTime.now()+"-firstFactory:"+ data);
+            System.out.println(LocalDateTime.now() + "-firstFactory:" + new String(message.getBody()));
             Thread.sleep(10);
-        }catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
+        } finally {
+            channel.basicNack(message.getMessageProperties().getDeliveryTag(), false, false);
         }
     }
 
-    @RabbitListener(queues = "testSecond", containerFactory="secondFactory")
-    public void testSecond(Object data) {
+    @RabbitListener(queues = "testSecond", containerFactory = "secondFactory")
+    public void testSecond(Channel channel, Message message) throws IOException {
         try {
-            System.out.println(LocalDateTime.now()+"-secondFactory:"+ data);
+            System.out.println(LocalDateTime.now() + "-secondFactory:" + new String(message.getBody()));
             Thread.sleep(10);
-        }catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
+        } finally {
+            channel.basicNack(message.getMessageProperties().getDeliveryTag(), false, false);
         }
     }
 }
