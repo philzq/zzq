113b7546af18bb52c0d2fcf87ab8b37afb8e4c70
zhiqiangzhou <zhiqiangzhou@ctrip.com>
2021-08-19 17:10:26 +0800

    rabbitmq

--- spring-boot/spring-boot-ribbitmq/src/main/java/zzq/consumption/RabbitMqMessage.java
+++ spring-boot/spring-boot-ribbitmq/src/main/java/zzq/consumption/RabbitMqMessage.java
@@ -3,13 +3,15 @@ package zzq.consumption;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
 
+import java.time.LocalDateTime;
+
 @Component
 public class RabbitMqMessage {
 
     @RabbitListener(queues = "testFirst", containerFactory="firstFactory")
     public void testFirst(Object data) {
         try {
-            System.out.println("firstFactory:"+ data);
+            System.out.println(LocalDateTime.now()+"-firstFactory:"+ data);
             Thread.sleep(10);
         }catch (Exception e){
             e.printStackTrace();
@@ -19,7 +21,7 @@ public class RabbitMqMessage {
     @RabbitListener(queues = "testSecond", containerFactory="secondFactory")
     public void testSecond(Object data) {
         try {
-            System.out.println("secondFactory:"+ data);
+            System.out.println(LocalDateTime.now()+"-secondFactory:"+ data);
             Thread.sleep(10);
         }catch (Exception e){
             e.printStackTrace();
