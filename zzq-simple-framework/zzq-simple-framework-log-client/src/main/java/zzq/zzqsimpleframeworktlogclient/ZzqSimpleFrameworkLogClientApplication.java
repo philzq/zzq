00cd1abe98244c80e233810a2202c367fe54c3ae
zhouzhiqiang <18770911080>
2023-02-23 15:56:50 +0800

    log demo

--- zzq-simple-framework/zzq-simple-framework-log-client/src/main/java/zzq/zzqsimpleframeworktlogclient/ZzqSimpleFrameworkLogClientApplication.java
+++ zzq-simple-framework/zzq-simple-framework-log-client/src/main/java/zzq/zzqsimpleframeworktlogclient/ZzqSimpleFrameworkLogClientApplication.java
@@ -3,6 +3,10 @@ package zzq.zzqsimpleframeworktlogclient;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import zzq.zzqsimpleframeworkjson.config.EnableJacksonConfigure;
+import zzq.zzqsimpleframeworklog.LogUtilFactory;
+import zzq.zzqsimpleframeworklog.entity.SystemInfoLogEntity;
+
+import javax.annotation.PostConstruct;
 
 /**
  * @author zhouzhiqiang
@@ -16,5 +20,14 @@ public class ZzqSimpleFrameworkLogClientApplication {
     public static void main(String[] args) {
         System.setProperty("appName", "zzq-simple-framework-log-client");
         SpringApplication.run(ZzqSimpleFrameworkLogClientApplication.class, args);
+    }
+    
+    @PostConstruct
+    public void test(){
+        SystemInfoLogEntity systemInfoLogEntity = SystemInfoLogEntity.builder()
+                .message("message")
+                .build();
+        LogUtilFactory.SYSTEM_INFO.info("title","132{}456{}789{}","***1",null,systemInfoLogEntity);
+        System.out.println();
     }
 }
